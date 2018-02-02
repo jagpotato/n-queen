59a03f2d04978bccc23ed406142754f0fcceadbb
daiki <dik.piyo.piyo@gmail.com>
2018-02-03 02:34:35 +0900

    find a solution

--- js/main.js
+++ js/main.js
@@ -1,71 +1,123 @@
-const N = 3;
-let queenPositions = new Array(N);
 
-nQueen();
-
-function nQueen() {
-  setQueens();
-  console.log(queenPositions);
-  searchQueens(0);
-
-  // console.log(queenPositions);
-}
-
-function setQueens() {
-  // queenPositions.push(0);
-  for ( let i = 0; i < N; i++ ) {
-    queenPositions[i] = i;
-  }
-}
-
-function searchQueens(i) {
-  if ( i === N ) {
-    console.log(queenPositions);
-    return;
-  }
-  let tmp;
-  for ( j = 0; j < N; j++ ) {
-    tmp = queenPositions[i];
-    queenPositions[i] = queenPositions[j];
-    queenPositions[j] = tmp;
-    searchQueens(i + 1);
-    tmp = queenPositions[i];
-    queenPositions[i] = queenPositions[j];
-    queenPositions[j] = tmp;
-  }
-}
-
-
+// http://vivi.dyndns.org/tech/puzzle/NQueen.html
 
 // nQueen(8);
-//
+
 // function nQueen(n) {
 //   let queenPositions = [];
-//   setQueens(queenPositions, n);
-//   searchQueens(queenPositions, n, 0);
-//   // console.log(queenPositions);
+//   let rup = [];
+//   let rdn = [];
+//   initArray(queenPositions, rup, rdn, n);
+//   searchQueens(queenPositions, rup, rdn, n, 0);
+//   // console.log(queenPositions, rup, rdn);
 // }
-//
-// function setQueens(queenPositions, n) {
-//   // queenPositions.push(0);
+
+// function initArray(queenPositions, rup, rdn, n) {
+//   if ( queenPositions.length > 0 ) {
+//     while ( queenPositions.length > 0 ) {
+//       queenPositions.pop();
+//     }
+//   }
+//   if ( rup.length > 0 ) {
+//     while ( rup.length > 0 ) {
+//       rup.pop();
+//     }
+//   }
+//   if ( rdn.length > 0 ) {
+//     while ( rdn.length > 0 ) {
+//       rdn.pop();
+//     }
+//   }
 //   for ( let i = 0; i < n; i++ ) {
-//     queenPositions.push(i);
+//     queenPositions.push(-1);
+//   }
+//   for ( let i = 0; i < (2 * n - 1); i++ ) {
+//     rup.push(false);
+//   }
+//   for ( let i = 0; i < (2 * n - 1); i++ ) {
+//     rdn.push(false);
 //   }
 // }
-//
-// function searchQueens(queenPositions, n, i) {
-//   console.log(queenPositions);
-//   if ( i > n ) {
-//     return;
-//   }
-//   let tmp;
-//   for ( j = i; j < n; j++ ) {
-//     tmp = queenPositions[i];
-//     queenPositions[i] = queenPositions[j];
-//     queenPositions[j] = tmp;
-//     searchQueens(queenPositions, n, i + 1);
-//     tmp = queenPositions[i];
-//     queenPositions[i] = queenPositions[j];
-//     queenPositions[j] = tmp;
+
+// function searchQueens(queenPositions, rup, rdn, n, i) {  
+//   for ( let j = 0; j < n; j++ ) {
+//     if ( queenPositions[j] < 0 && rup[i + j] === false && rdn[j - i + n - 1] === false ) {
+//       if ( i + 1 === n ) {
+//         console.log("a");
+//       } else {
+//         queenPositions[j] = i;
+//         rup[i + j] = true;
+//         rdn[j - i + n - 1] = true;
+//         searchQueens(queenPositions, rup, rdn, n, i + 1);
+//         queenPositions[j] = -1;
+//         rup[i + j] = false;
+//         rdn[j - i + n - 1] = false;
+//       }
+//     }
 //   }
 // }
+
+
+
+nQueen(8);
+
+function nQueen(n) {
+  let queenPositions = [];
+  let rup = [];
+  let rdn = [];  
+  initArray(queenPositions, rup, rdn, n);
+  searchQueens(queenPositions, rup, rdn, n, 0);
+}
+
+function initArray(queenPositions, rup, rdn, n) {
+  for ( let i = 0; i < n; i++ ) {
+    queenPositions.push(-1);
+  }
+  for ( let i = 0; i < (2 * n - 1); i++ ) {
+    rup.push(false);
+  }
+  for ( let i = 0; i < (2 * n - 1); i++ ) {
+    rdn.push(false);
+  }
+}
+
+function searchQueens(queenPositions, rup, rdn, n, i) {
+  let stack = [];
+  stack.push({"pos": queenPositions, "rup": rup, "rdn": rdn, "n": n, "i": i});
+  let answer = false;
+  
+  while ( stack.length > 0 ) {
+    if ( answer === true ) {
+      break;
+    }
+
+    let topIndex = stack.length - 1;
+    queenPositions = stack[topIndex].pos.slice();
+    rup = stack[topIndex].rup.slice();
+    rdn = stack[topIndex].rdn.slice();
+    n = stack[topIndex].n;
+    i = stack[topIndex].i;  
+    stack.pop();
+
+    for ( let j = 0; j < n; j++ ) {
+      if ( queenPositions[j] < 0 && rup[i + j] === false && rdn[j - i + n - 1] === false ) {
+        if ( i + 1 === n ) {
+          queenPositions[j] = i;
+          console.log(queenPositions);
+          answer = true;
+        } else {
+          queenPositions[j] = i;
+          rup[i + j] = true;
+          rdn[j - i + n - 1] = true;
+          stack.push({"pos": queenPositions.slice(), "rup": rup.slice(), "rdn": rdn.slice(), "n": n, "i": i + 1});
+          queenPositions[j] = -1;
+          rup[i + j] = false;
+          rdn[j - i + n - 1] = false;
+        }
+      }
+    }
+  }
+}
+
+
+
